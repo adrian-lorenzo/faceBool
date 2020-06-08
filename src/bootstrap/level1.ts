4f000f318c27d18cf0e0d99f1bd82e0825b8f86b
Adrián Lorenzo Melián <adrianlorenzomelian@gmail.com>
2020-06-08 18:25:04 +0100

    Integrated scroll to physics loop. Refactor. Add new user actions.

--- src/bootstrap/level1.ts
+++ src/bootstrap/level1.ts
@@ -67,7 +67,61 @@ const level1 = new Level([
                 height: relHeight(0.01)
             }
         )
+    ]),
+
+    new Stage([
+        new Platform(
+            {
+                x: relWidth(0.25),
+                y: relHeight(0.6)
+            },
+            {
+                width: relWidth(0.15), 
+                height: relHeight(0.05)
+            },
+            0.39
+        ),
+        new Platform(
+            {
+                x: relWidth(0.9),
+                y: relHeight(0.4)
+            },
+            {
+                width: relWidth(0.3),
+                height: relHeight(0.05)
+            }
+        ),
+        new Platform(
+            {
+                x: relWidth(0.5), 
+                y: relHeight(0.999)
+            }, 
+            {
+                width: relWidth(1), 
+                height: relHeight(0.01)
+            }
+        ),
+        new Platform(
+            {
+                x: relWidth(0.001), 
+                y: relHeight(0.5)
+            },
+            {
+                width: relWidth(0.01), 
+                height: relHeight(1)
+            }
+        ),
+        new Platform(
+            {
+                x: relWidth(0.5),  
+                y: relHeight(0.001)
+            },
+            {
+                width: relWidth(1),
+                height: relHeight(0.01)
+            }
+        )
     ])
-], 60);
+], 30);
 
 export default level1;
