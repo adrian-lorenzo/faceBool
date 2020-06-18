8c505fcb22073cd2f752a12ffc784de4ded4429a
unknown <borjia98@gmail.com>
2020-06-18 19:35:31 +0100

    stop time on pause

--- src/components/Sketch.ts
+++ src/components/Sketch.ts
@@ -1,24 +1,24 @@
+import { IpcRenderer, Remote } from "electron";
 import P5 from "p5";
-import { PlayerAction } from "../models/PlayerAction";
-import FaceDetectionService from "../services/FaceDetectionService";
-import { relHeight, relWidth } from "../utils/uiUtils";
-import { Loader } from "./Loader";
-import { GameStates, MainScreen, DieScreen, WinScreen, TutorialScreen } from "./Screen";
-import { Sound } from "./Sound";
 import { Vec2 } from "planck-js";
 import level1Builder from "../bootstrap/level1";
 import level2Builder from "../bootstrap/level2";
 import level3Builder from "../bootstrap/level3";
+import { PlayerAction } from "../models/PlayerAction";
+import FaceDetectionService from "../services/FaceDetectionService";
+import { relHeight, relWidth } from "../utils/uiUtils";
+import { Loader } from "./Loader";
 import { PauseMenu } from "./PauseMenu";
-import { Remote, IpcRenderer } from "electron";
+import { DieScreen, GameStates, MainScreen, TutorialScreen, WinScreen } from "./Screen";
+import { Sound } from "./Sound";
 
 declare global {
-  interface Window {
-    require: (module: 'electron') => {
-        ipcRenderer: IpcRenderer
-        remote: Remote
-    };
-  }
+    interface Window {
+        require: (module: 'electron') => {
+            ipcRenderer: IpcRenderer
+            remote: Remote
+        };
+    }
 }
 
 const { ipcRenderer, remote } = window.require('electron');
@@ -34,6 +34,7 @@ const Sketch = (p5: P5) => {
     let shader;
     let shaderTexture;
     let time = 0;
+    let timePaused = 0;
 
     const maxTime = 60 * 1000;
     const platformTexture = p5.loadImage('textures/platform_texture.jpg');
@@ -68,10 +69,10 @@ const Sketch = (p5: P5) => {
         p5.createCanvas(relWidth(1), relHeight(1), p5.WEBGL);
 
         state = GameStates.MENU;
-        menu  = new MainScreen(fontTitle, p5.loadImage('textures/baloncesto.png'));
+        menu = new MainScreen(fontTitle, p5.loadImage('textures/baloncesto.png'));
         dieScreen = new DieScreen(fontTitle, p5.loadImage('textures/death.png'));
         winScreen = new WinScreen(fontTitle, p5.loadImage('textures/win.png'));
-        tutorial  = new TutorialScreen(fontTitle, font);
+        tutorial = new TutorialScreen(fontTitle, font);
         sound = new Sound();
 
         videoCapture = p5.createCapture(p5.VIDEO);
@@ -84,41 +85,41 @@ const Sketch = (p5: P5) => {
         setTimeout(() => {
             currentLevel.hasStarted = true;
             hasEverythingLoaded = true;
-            time = Date.now();
+            // time = Date.now();
             p5.tint(255, 255);
         }, 4000);
     }
 
     p5.draw = () => {
         // Environment
-        if(state === GameStates.MENU){
+        if (state === GameStates.MENU) {
             sound.playMenuMusic();
             menu.draw(p5);
-        } else if (state === GameStates.GAME){
+        } else if (state === GameStates.GAME) {
             p5.translate(-p5.width / 2, -p5.height / 2, 0);
             drawBackground();
-            
+
             if (!hasEverythingLoaded) loader.draw(p5);
             runDetection();
             currentLevel.run(p5, ballTexture, platformTexture);
             sound.playGameMusic();
 
-            if(currentLevel.checkIfPLayerIsDead()){
+            if (currentLevel.checkIfPLayerIsDead()) {
                 sound.stopGameMusic();
                 sound.playLoseSound();
                 state = GameStates.DIE;
                 restartLevel();
             }
-        } else if (state === GameStates.DIE){
+        } else if (state === GameStates.DIE) {
             dieScreen.draw(p5);
         } else if (state === GameStates.PAUSE) {
             p5.translate(-p5.width / 2, -p5.height / 2, 0);
             drawBackground();
             currentLevel.run(p5, ballTexture, platformTexture);
             pauseMenu.draw(p5);
-        } else if (state === GameStates.WIN){
+        } else if (state === GameStates.WIN) {
             winScreen.draw(p5);
-        } else if (state === GameStates.TUTORIAL){
+        } else if (state === GameStates.TUTORIAL) {
             tutorial.draw(p5);
         }
     }
@@ -134,11 +135,11 @@ const Sketch = (p5: P5) => {
             if (p5.key === 'D' || p5.key === 'd') {
                 currentLevel.actions.set(PlayerAction.MoveRight, true);
             }
-    
+
             if (p5.key === 'A' || p5.key === 'a') {
                 currentLevel.actions.set(PlayerAction.MoveLeft, true);
             }
-    
+
             if (p5.key === 'W' || p5.key === 'w') {
                 currentLevel.actions.set(PlayerAction.Jump, true);
             }
@@ -147,6 +148,7 @@ const Sketch = (p5: P5) => {
                 state = GameStates.PAUSE;
                 sound.stopGameMusic();
                 p5.tint(55, 100);
+                timePaused = Date.now();
                 currentLevel.isPaused = true;
             }
         }
@@ -155,24 +157,28 @@ const Sketch = (p5: P5) => {
             if (p5.key === 'Q' || p5.key === 'q') {
                 remote.app.quit();
             }
-    
+
             if (p5.key === 'R' || p5.key === 'r') {
                 restartLevel();
                 state = GameStates.GAME;
                 p5.tint(255, 255);
                 currentLevel.isPaused = false;
+                time += Date.now() - timePaused;
+                timePaused = 0;
             }
-    
+
             if (p5.keyCode === p5.ESCAPE) {
                 state = GameStates.GAME;
                 p5.tint(255, 255);
+                time += Date.now() - timePaused;
                 currentLevel.isPaused = false;
+                timePaused = 0;
             }
         }
 
         if (p5.keyCode === p5.ENTER) {
             if (state === GameStates.MENU) {
-                if(menu.indexOption === 0) {
+                if (menu.indexOption === 0) {
                     state = GameStates.TUTORIAL;
                 } else {
                     levelBuildersIdx = menu.indexOption - 1;
@@ -180,8 +186,8 @@ const Sketch = (p5: P5) => {
                     currentLevel.hasStarted = true;
                     state = GameStates.GAME;
                     sound.stopMenuMusic();
-                }   
-            } else if(state === GameStates.TUTORIAL){
+                }
+            } else if (state === GameStates.TUTORIAL) {
                 state = GameStates.MENU;
             }
         }
@@ -194,23 +200,23 @@ const Sketch = (p5: P5) => {
             menu.changeOption(1);
         }
 
-        if ( p5.keyCode === p5.ENTER && state === GameStates.WIN) {
+        if (p5.keyCode === p5.ENTER && state === GameStates.WIN) {
             state = GameStates.MENU;
         }
 
-        if ( (p5.key === 'y' || p5.key === 'Y') && state === GameStates.DIE) {
+        if ((p5.key === 'y' || p5.key === 'Y') && state === GameStates.DIE) {
             state = GameStates.GAME;
             dieScreen.resetCount();
             sound.stopLoseMusic();
         }
 
-        if ( (p5.key === 'n' || p5.key === 'N') && state === GameStates.DIE) {
+        if ((p5.key === 'n' || p5.key === 'N') && state === GameStates.DIE) {
             state = GameStates.MENU;
             dieScreen.resetCount();
             sound.stopLoseMusic();
         }
 
-        if((p5.key === 'p' || p5.key === 'P') && state === GameStates.GAME){
+        if ((p5.key === 'p' || p5.key === 'P') && state === GameStates.GAME) {
             sound.pauseGameMusic();
         }
     }
@@ -233,6 +239,7 @@ const Sketch = (p5: P5) => {
     function restartLevel() {
         currentLevel = levelBuilders[levelBuildersIdx]();
         currentLevel.hasStarted = true;
+        time = Date.now();
     }
 
     function drawBackground() {
@@ -243,7 +250,7 @@ const Sketch = (p5: P5) => {
         if (shader && shaderTexture) {
             shader.setUniform('tex0', videoCapture);
             shader.setUniform('resolution', [relWidth(1), relHeight(1)]);
-            shader.setUniform('current_time', Date.now() - time);
+            shader.setUniform('current_time', timePaused > 0 ? 0 : Date.now() - time);
             shader.setUniform('max_time', maxTime);
             shaderTexture.shader(shader);
             shaderTexture.rect(0, 0, relWidth(1), relHeight(1));
@@ -262,8 +269,8 @@ const Sketch = (p5: P5) => {
                     if (detection) {
                         const scaleX = dimensions.width / faceDetectionService.imageDimension.width;
                         const scaleY = dimensions.height / faceDetectionService.imageDimension.height;
-                        let leftEye = { x: detection[1][0] * scaleX, y: detection[1][1] * scaleY};
-                        let rightEye = { x: detection[0][0] * scaleX, y: detection[0][1] * scaleY};
+                        let leftEye = { x: detection[1][0] * scaleX, y: detection[1][1] * scaleY };
+                        let rightEye = { x: detection[0][0] * scaleX, y: detection[0][1] * scaleY };
                         let position = Vec2((leftEye.x + rightEye.x) / 2., (leftEye.y + rightEye.y) / 2.);
                         let direction = Vec2(leftEye.x - rightEye.x, leftEye.y - rightEye.y);
 
