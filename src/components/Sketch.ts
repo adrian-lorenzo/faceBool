bfd280a29a23eb437ae5400bfce25f858de997ef
miguel-kjh <miguelangel.kjh@gmail.com>
2020-06-18 17:39:40 +0100

    update

--- src/components/Sketch.ts
+++ src/components/Sketch.ts
@@ -52,7 +52,7 @@ const Sketch = (p5: P5) => {
         // Canvas setup
         p5.createCanvas(relWidth(1), relHeight(1), p5.WEBGL);
 
-        state = GameStates.WIN;
+        state = GameStates.MENU;
         menu  = new MainScreen(fontTitle, p5.loadImage('textures/baloncesto.png'));
         dieScreen = new DieScreen(fontTitle, p5.loadImage('textures/death.png'));
         winScreen = new WinScreen(fontTitle, p5.loadImage('textures/win.png'));
